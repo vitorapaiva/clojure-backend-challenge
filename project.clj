8c48a84c00169dc8d5afed1cf177cca419d22e41
Vitor Paiva <vitor.paiva@picpay.com>
2021-06-27 18:30:14 -0300

    First commit

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject clojure-backend-challenge "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [compojure "1.6.2"]
+                 [ring "1.9.3"]
+                 [korma "0.4.3"]
+                 [mysql/mysql-connector-java "8.0.25"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler clojure-backend-challenge.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}}
+  :repl-options {:init-ns clojure-backend-challenge.core})
+
---
+++ test/clojure_backend_challenge/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-backend-challenge.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-backend-challenge.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
